69c390a8b7cc7a087c3f5e154aefd1b25d00b91a
Tuncay456 <106577872+Tuncay456@users.noreply.github.com>
2022-06-06 21:50:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from os import getenv
 
 API_ID = 5891777
 API_HASH = '64fa91f5fafd43a3b9dc504f0e2a4d70'
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("5568843844:AAFfT4yTLaZbTiBU-NbIIETWJ0avbdlPEPs")
